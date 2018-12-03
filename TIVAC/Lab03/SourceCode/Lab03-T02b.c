6cf87950e8719600daee57002f38e8a1b4e60b59
dyej1 <32071790+dyej1@users.noreply.github.com>
2018-12-02 22:27:48 -0800

    Add files via upload

---
+++ TIVAC/Lab03/SourceCode/Lab03-T02b.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+uint8_t ui8PinData=2;
+
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_9|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+
+    while(1) {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, ui8PinData);
+        SysCtlDelay(2000000);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
+        SysCtlDelay(2000000);
+        if(ui8PinData==2) {ui8PinData=8;} else {
+            if(ui8PinData==8) {ui8PinData=4;} else {
+                if(ui8PinData==4) {ui8PinData=10;} else {
+                    if(ui8PinData==10) {ui8PinData=6;} else {
+                        if(ui8PinData==6) {ui8PinData=12;} else {
+                            if(ui8PinData==12) {ui8PinData=14;} else {ui8PinData=2;}}}}}}
+    }
+}
